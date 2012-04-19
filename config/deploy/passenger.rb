b46b71d0a50a74ecb055a84c644418f5d16eee72
Thies Schuster <schuster@redrauscher.de>
2012-04-20 00:20:53 +0200

    helping unicorn start

--- config/deploy/passenger.rb
+++
@@ -1,7 +0,0 @@
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-end
