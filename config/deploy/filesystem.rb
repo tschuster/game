a999a91ae1acda54bce462af7e114dbc00ad532b
Thies Schuster <schuster@redrauscher.de>
2012-04-24 23:12:02 +0200

    fixed cleanupcaptask to not use sudo

--- config/deploy/filesystem.rb
+++ config/deploy/filesystem.rb
@@ -18,6 +18,20 @@ namespace :deploy do
     files_to_link.each                { |source, target| run "ln -s #{source} #{target}"}
     chmods_to_set.each                { |target, chmod| run "chmod #{chmod} #{target}" }
   end
+
+  task :cleanup, :except => { :no_release => true } do
+    count = fetch(:keep_releases, 5).to_i
+    local_releases = capture("ls -xt #{releases_path}").split.reverse
+    if count >= local_releases.length
+      logger.important "no old releases to clean up"
+    else
+      logger.info "keeping #{count} of #{local_releases.length} deployed releases"
+      directories = (local_releases - local_releases.last(count)).map { |release|
+        File.join(releases_path, release) }.join(" ")
+
+      run "rm -rf #{directories}"
+    end
+  end
 end
 
 before 'deploy:finalize_update', 'deploy:symlink_dependencies'
