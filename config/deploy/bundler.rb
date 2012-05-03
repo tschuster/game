a96e05bad6ecf18df3fe6979b6efa743a1bf6d1c
Thies Schuster <schuster@redrauscher.de>
2012-05-03 22:00:45 +0200

    recipe for restarting job daemon

--- config/deploy/bundler.rb
+++
@@ -1,8 +0,0 @@
-# taken and adapted from bundler
-namespace :bundle do
-  task :install do
-    run "bundle install --gemfile #{release_path}/Gemfile --path #{fetch(:bundle_dir, "#{shared_path}/bundle")} --deployment --without development test cucumber"
-  end
-end
-
-#after "deploy:update_code", "bundle:install"
---
+++ config/deploy/daemon.rb
@@ -0,0 +1,7 @@
+namespace :daemon do
+  task :restart do
+    run "svc -du ~/service/job-daemon"
+  end
+end
+
+#after "deploy:update_code", "daemon:restart"
