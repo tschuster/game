28f6821bb8d2737146dd4a6f83b9457d97511853
Thies Schuster <schuster@redrauscher.de>
2012-05-04 16:36:30 +0200

    didn't work

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,32 @@
 Game::Application.routes.draw do
 
-  scope "/game" do
-    devise_for :users
+  devise_for :users
 
-    resources :game, :only => :index
+  resources :game, :only => :index
 
-    resources :users, :only => [ :show, :index ]
+  resources :users, :only => [ :show, :index ]
 
-    resources :notifications, :only => [ :index ] do
-      collection do
-        put :read
-      end
+  resources :notifications, :only => [ :index ] do
+    collection do
+      put :read
     end
-
-    resources :jobs, :only => [ :index ] do
-      member do
-        get 'accept'
-      end
-    end
-
-    resources :actions, :only => [:index, :show, :create, :destroy] do
-      collection do
-        get 'current'
-        get 'recent'
-      end
-    end
-
-    match 'operations' => 'actions#operations', :as => :operations
-
-    root :to => 'game#index'
-    match "/" => 'game#index'
   end
+
+  resources :jobs, :only => [ :index ] do
+    member do
+      get 'accept'
+    end
+  end
+
+  resources :actions, :only => [:index, :show, :create, :destroy] do
+    collection do
+      get 'current'
+      get 'recent'
+    end
+  end
+
+  match 'operations' => 'actions#operations', :as => :operations
+
+  root :to => 'game#index'
+  match "/" => 'game#index'
 end
