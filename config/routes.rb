da1863536f6dd0bce8406aba3fa00f54b955d8ec
Thies Schuster <schuster@redrauscher.de>
2012-05-04 16:29:01 +0200

    routes for game prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,34 @@
 Game::Application.routes.draw do
 
-  devise_for :users
+  scope "/game" do
+    devise_for :users
 
-  resources :game, :only => :index
+    resources :game, :only => :index
 
-  resources :users, :only => [ :show, :index ]
+    resources :users, :only => [ :show, :index ]
 
-  resources :notifications, :only => [ :index ] do
-    collection do
-      put :read
+    resources :notifications, :only => [ :index ] do
+      collection do
+        put :read
+      end
     end
-  end
 
-  resources :jobs, :only => [ :index ] do
-    member do
-      get 'accept'
+    resources :jobs, :only => [ :index ] do
+      member do
+        get 'accept'
+      end
     end
-  end
 
-  resources :actions, :only => [:index, :show, :create, :destroy] do
-    collection do
-      get 'current'
-      get 'recent'
+    resources :actions, :only => [:index, :show, :create, :destroy] do
+      collection do
+        get 'current'
+        get 'recent'
+      end
     end
+
+    match 'operations' => 'actions#operations', :as => :operations
+
+    root :to => 'game#index'
+    match "/" => 'game#index'
   end
-
-  match 'operations' => 'actions#operations', :as => :operations
-
-  root :to => 'game#index'
-  match "/" => 'game#index'
 end
