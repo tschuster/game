d8b4eadbf58a34d62bed68aa63b855b577687fd8
Thies Schuster <schuster@redrauscher.de>
2012-05-09 18:58:49 +0200

    small fixes

--- app/models/action.rb
+++ app/models/action.rb
@@ -23,18 +23,19 @@ class Action < ActiveRecord::Base
     }
   }
 
-  TYPE_BOTNET_EVOLVE  = 101
-  TYPE_BOTNET_BUY     = 102
-  TYPE_HACKING_EVOLVE = 103
-  TYPE_HACKING_BUY    = 104
-  TYPE_DEFENSE_EVOLVE = 105
-  TYPE_DEFENSE_BUY    = 106
-  TYPE_PERFORM_JOB    = 201
-  TYPE_ATTACK_USER    = 301
-  TYPE_ATTACK_COMPANY = 302
-  TYPE_ARREST         = 401
-  TYPE_SYSTEM_CRASH   = 402
-  TYPE_DDOS_CRASH     = 403
+  TYPE_BOTNET_EVOLVE    = 101
+  TYPE_BOTNET_BUY       = 102
+  TYPE_HACKING_EVOLVE   = 103
+  TYPE_HACKING_BUY      = 104
+  TYPE_DEFENSE_EVOLVE   = 105
+  TYPE_DEFENSE_BUY      = 106
+  TYPE_PERFORM_JOB      = 201
+  TYPE_ATTACK_USER      = 301
+  TYPE_ATTACK_COMPANY   = 302
+  TYPE_ATTACK_USER_DDOS = 303
+  TYPE_ARREST           = 401
+  TYPE_SYSTEM_CRASH     = 402
+  TYPE_DDOS_CRASH       = 403
 
   def perform!
     # eigenes Botnet erweitern
@@ -70,11 +71,11 @@ class Action < ActiveRecord::Base
       user.attack(target, :hack)
 
     # User dDoS angreifen
-    elsif type_id == Action::TYPE_DDOS_CRASH
+    elsif type_id == Action::TYPE_ATTACK_USER_DDOS
       user.attack(target, :ddos)
 
     # Sperre aufheben
-    elsif type_id == Action::TYPE_SYSTEM_CRASH
+    elsif (type_id == Action::TYPE_SYSTEM_CRASH || type_id == Action::TYPE_DDOS_CRASH )
       # n/a
 
     else
@@ -105,8 +106,10 @@ class Action < ActiveRecord::Base
       "Performing Job"
     when Action::TYPE_ATTACK_USER
       "Performing Hacking-Attack on #{target.present? ? target.nickname : "another Hacker"}"
-    when Action::TYPE_DDOS_CRASH
+    when Action::TYPE_ATTACK_USER_DDOS
       "Performing dDoS-Attack on #{target.present? ? target.nickname : "another Hacker"}"
+    when Action::TYPE_DDOS_CRASH
+      "Rebooting System after dDoS-Attack"
     when Action::TYPE_SYSTEM_CRASH
       "Rebooting System"
     else
@@ -116,6 +119,10 @@ class Action < ActiveRecord::Base
 
   def time_remaining
     [completed_at - DateTime.now, 0].max
+  end
+
+  def can_be_canceled?
+    type_id != Action::TYPE_SYSTEM_CRASH && type_id != Action::TYPE_DDOS_CRASH
   end
 
   class << self
@@ -153,7 +160,7 @@ class Action < ActiveRecord::Base
         target_type = "User"
         DateTime.now + current_user.time_to_attack(target, :hack).seconds
 
-      elsif action.type_id == Action::TYPE_DDOS_CRASH
+      elsif action.type_id == Action::TYPE_ATTACK_USER_DDOS
         target = User.where(:id => action.target_id).first
         return if target.blank?
         target_type = "User"
