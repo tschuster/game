2dcf1373b55e46024ee75eb1f9ac1de79f9b3cf0
Thies Schuster <schuster@redrauscher.de>
2012-05-16 09:05:27 +0200

    bugfix skill-leveling

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,35 +51,35 @@ class User < ActiveRecord::Base
 
   # eigenes Botnet erweitern
   def evolve_botnet
-    update_attribute(:botnet_ratio, botnet_ratio + CONFIG["botnet"]["evolve_ratio"].to_i)
+    update_attribute(:botnet_ratio, botnet_ratio_without_bonus + CONFIG["botnet"]["evolve_ratio"].to_i)
   end
 
   # Botnet-Erweiterung dazukaufen
   def buy_botnet
     return if money < CONFIG["botnet"]["buy_cost"].to_i
-    update_attributes(:money => [0, (money-CONFIG["botnet"]["buy_cost"].to_i)].max, :botnet_ratio => botnet_ratio + CONFIG["botnet"]["buy_ratio"].to_i)
+    update_attributes(:money => [0, (money-CONFIG["botnet"]["buy_cost"].to_i)].max, :botnet_ratio => botnet_ratio_without_bonus + CONFIG["botnet"]["buy_ratio"].to_i)
   end
 
   # eigene Hacking-Skills erweitern
   def evolve_hacking
-    update_attribute(:hacking_ratio, hacking_ratio + CONFIG["hacking"]["evolve_ratio"].to_i)
+    update_attribute(:hacking_ratio, hacking_ratio_without_bonus + CONFIG["hacking"]["evolve_ratio"].to_i)
   end
 
   # Hacking-Skill dazukaufen
   def buy_hacking
     return if money < CONFIG["hacking"]["buy_cost"].to_i
-    update_attributes(:money => [0, (money-CONFIG["hacking"]["buy_cost"].to_i)].max, :hacking_ratio => hacking_ratio + CONFIG["hacking"]["buy_ratio"].to_i)
+    update_attributes(:money => [0, (money-CONFIG["hacking"]["buy_cost"].to_i)].max, :hacking_ratio => hacking_ratio_without_bonus + CONFIG["hacking"]["buy_ratio"].to_i)
   end
 
   # eigene Verteidigung erweitern
   def evolve_defense
-    update_attribute(:defense_ratio, defense_ratio + CONFIG["defense"]["evolve_ratio"].to_i)
+    update_attribute(:defense_ratio, defense_ratio_without_bonus + CONFIG["defense"]["evolve_ratio"].to_i)
   end
 
   # Hacking-Skill dazukaufen
   def buy_defense
     return if money < CONFIG["defense"]["buy_cost"].to_i
-    update_attributes(:money => [0, (money-CONFIG["defense"]["buy_cost"].to_i)].max, :defense_ratio => defense_ratio + CONFIG["defense"]["buy_ratio"].to_i)
+    update_attributes(:money => [0, (money-CONFIG["defense"]["buy_cost"].to_i)].max, :defense_ratio => defense_ratio_without_bonus + CONFIG["defense"]["buy_ratio"].to_i)
   end
 
   def has_purchased?(equipment)
