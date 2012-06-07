23d1a10ad9725a07595e272b6cec488bd291e246
Thies Schuster <schuster@redrauscher.de>
2012-06-08 00:20:10 +0200

    more ActionShell action!

--- app/controllers/action_shell_controller.rb
+++ app/controllers/action_shell_controller.rb
@@ -12,7 +12,7 @@ class ActionShellController < ApplicationController
       flash[:notice] = "ActionShell session ended"
       render :js => "$('#results_container').hide();document.location.href = '#{game_index_path}';"
     else
-      @shell.perform! params[:command].to_s.squish
+      @shell.perform!(params[:command].to_s.squish, params[:delegate])
       render :layout => false
     end
   end
@@ -21,22 +21,28 @@ class ActionShellController < ApplicationController
 
     def set_session_from_shell
       session[:action_shell] = {
-        :history => @shell.history,
-        :result => @shell.result,
-        :current_dir => @shell.current_dir,
-        :file_system => @shell.file_system,
-        :clients => @shell.clients
+        :history            => @shell.history,
+        :result             => @shell.result,
+        :current_dir        => @shell.current_dir,
+        :file_system        => @shell.file_system,
+        :current_connection => @shell.current_connection,
+        :password_retry     => @shell.password_retry,
+        :authenticated      => @shell.authenticated,
+        :clients            => @shell.clients
       }
     end
 
     def set_shell_from_session
       if session[:action_shell].present?
         @shell = ActionShell.new(
-          :history      => session[:action_shell][:history],
-          :current_dir  => session[:action_shell][:current_dir],
-          :file_system  => session[:action_shell][:file_system],
-          :clients      => session[:action_shell][:clients],
-          :current_user => current_user
+          :history            => session[:action_shell][:history],
+          :current_dir        => session[:action_shell][:current_dir],
+          :file_system        => session[:action_shell][:file_system],
+          :clients            => session[:action_shell][:clients],
+          :current_connection => session[:action_shell][:current_connection],
+          :password_retry     => session[:action_shell][:password_retry],
+          :authenticated      => session[:action_shell][:authenticated],
+          :current_user       => current_user
         )
       else
         @shell = ActionShell.new(:current_user => current_user)
--- app/models/action_shell.rb
+++ app/models/action_shell.rb
@@ -2,21 +2,34 @@
 class ActionShell
 
   def initialize(options={})
-    @history      = Array.wrap(options[:history])
-    @current_dir  = options[:current_dir]
-    @file_system  = options[:file_system]
-    @current_user = options[:current_user]
-    @clients      = options[:clients]
+    @history            = options[:history]
+    @current_dir        = options[:current_dir]
+    @file_system        = options[:file_system]
+    @current_user       = options[:current_user]
+    @current_connection = options[:current_connection]
+    @password_retry     = options[:password_retry]
+    @authenticated      = options[:authenticated]
+    @clients            = options[:clients]
   end
 
-  def perform!(command_string)
+  def perform!(command_string, delegate = nil)
     if command_string != "init"
       parsed_command = command_string.split(" ")
       command = parsed_command.slice!(0)
-      @history = "#{@current_dir} #{@current_user.nickname}$ #{command_string}"
-      if command.present?
-        @result = self.respond_to?(command.downcase) ? self.send(command.downcase, parsed_command) : "unrecognized command '#{command}'"
+
+      # if the input is to be delegated to a specific method...
+      if delegate.present?
+        @history = nil
+        @result = self.respond_to?(delegate.downcase) ? self.send(delegate.downcase, command) : nil
+
+      # the default command evaluation
+      else
+        @history = "#{@current_dir} #{@current_user.nickname}$ #{command_string}"
+        if command.present?
+          @result = self.respond_to?(command.downcase) ? self.send(command.downcase, parsed_command) : "unrecognized command '#{command}'"
+        end
       end
+      @result << "<script>$('.prompt').html('#{command_prompt}');</script>".html_safe if @result.present?
     else
       init!
     end
@@ -25,6 +38,8 @@ class ActionShell
   def init!
     @history = consolize("Initializing #{version}...=br==br=Type 'help' for help=br=")
     @result = nil
+    @current_connection = nil
+    @password_retry = 0
     build_file_system!
     build_clients!
   end
@@ -37,6 +52,10 @@ class ActionShell
     @current_dir
   end
 
+  def current_connection
+    @current_connection
+  end
+
   def file_system
     @file_system
   end
@@ -47,6 +66,32 @@ class ActionShell
 
   def clients
     @clients
+  end
+
+  def password_retry
+    @password_retry
+  end
+
+  def authenticated
+    @authenticated
+  end
+
+  def command_prompt
+    if @current_connection.present?
+      "#{@clients[@current_connection][:ip]}/#{@clients[@current_connection][:nickname]}$&nbsp;".html_safe
+    else
+      "#{@current_dir} #{@current_user.nickname}$&nbsp;".html_safe
+    end
+  end
+
+  def password_retry_limit_reached?
+    @password_retry >= 3
+  end
+
+  def reset_connection!
+    @password_retry = 0
+    @current_connection = nil
+    @authenticated = false
   end
 
   def consolize(object, options = {})
@@ -99,14 +144,20 @@ class ActionShell
     (@file_system.keys.delete_if { |path| @file_system[path] == :dir || !path.starts_with?(@current_dir) || path.gsub(@current_dir, "").include?("/") }).sort
   end
 
+  def random_password(length)
+    o = [('a'..'z'),('A'..'Z')].map{|i| i.to_a}.flatten
+    (0..length).map{o[rand(o.length)]}.join
+  end
+
   def build_clients!
     @clients = {}
     User.all.each do |u|
       next if u.id == @current_user.try(:id)
 
       random_ip = "#{rand(253)+1}.#{rand(256)}.#{rand(256)}.#{rand(253)+1}"
-      @clients[u.nickname] = { :nickname => u.nickname, :id => u.id, :ip => random_ip, :type => :user }
-      @clients[random_ip]  = { :nickname => u.nickname, :id => u.id, :ip => random_ip, :type => :ip }
+      password = random_password(10)
+      @clients[u.nickname] = { :nickname => u.nickname, :id => u.id, :ip => random_ip, :password => password, :type => :user }
+      @clients[random_ip]  = { :nickname => u.nickname, :id => u.id, :ip => random_ip, :password => password, :type => :ip }
     end
   end
 
@@ -142,7 +193,7 @@ class ActionShell
   end
 
   def version(p=nil)
-    "ActionShell v0.3 build 2012-06-06"
+    "ActionShell v0.4 build 2012-06-08"
   end
 
   def help(p=nil)
@@ -152,14 +203,14 @@ class ActionShell
       ["cd <dir>", "change directory to <dir>"],
       ["version", "show version information"],
       ["exit", "close ActionShell session and return to desktop"],
-      ["ping <name|ip>", "ping a client via name to resolve the ip, or via ip to resolve the name"]
+      ["ping <name|ip>", "ping a client via name to resolve the ip, or via ip to resolve the name"],
+      ["connect <ip>", "establish a connection to the given ip after entering the client's password"]
     ])
   end
 
   def ping(ip_or_nickname = nil)
     return "name or ip must be given" if ip_or_nickname.blank?
     ip_or_nickname = ip_or_nickname.first
-    Rails.logger.info(ip_or_nickname.to_yaml)
     result = @clients[ip_or_nickname]
     if result.present?
       if result[:type] == :ip
@@ -169,6 +220,70 @@ class ActionShell
       end
     else
       "client unknown: #{ip_or_nickname}"
+    end
+  end
+
+  def connect(ip)
+    return if @current_user.blank?
+    return "ip must be given" if ip.blank?
+    ip = ip.first
+    client = @clients[ip]
+    return "connection to #{ip} failed" if client.blank?
+    victim = User.find(client[:id])
+    return "connection to #{ip} failed" if victim.blank?
+
+    # Set current connection
+    @current_connection = client[:nickname]
+    @password_retry = 0
+
+    # The standard line length when both players have even strength
+    base_line_length = 75
+    delta = 25
+
+    # Attacker stronger than victim
+    if @current_user.hacking_ratio > victim.defense_ratio
+      percentage_stronger = (@current_user.hacking_ratio.to_f / [victim.defense_ratio.to_f, 1].max).round(2)-1
+      effective_line_legth = [(base_line_length-percentage_stronger*delta).round(0), 50].max
+
+    # Attacker weaker than victim
+    else
+      percentage_weaker = (victim.defense_ratio.to_f / [@current_user.hacking_ratio.to_f, 1].max).round(2)-1
+      effective_line_legth = [(base_line_length+percentage_stronger*delta).round(0), 100].min
+    end
+
+    # Display password stream
+    result = "Establishing connection to #{ip}...=br==br="
+    20.times do
+      line = random_password(rand(effective_line_legth - client[:password].length)) << client[:password]
+      line << random_password(effective_line_legth - line.length - 1)
+      line << "=br="
+      result << line
+    end
+    result << "=br=Password required!"
+    consolize(result) << "<script>$('#delegate').val('check_password');</script>".html_safe
+  end
+
+  def check_password(password)
+    return "No connection active" if @current_connection.blank?
+    if password_retry_limit_reached?
+      reset_connection!
+      return consolize("Password invalid!=br=Disconnected")
+    end
+    victim = @clients[@current_connection]
+
+    # Password incorrect - increase retry or disconnect
+    if password != victim[:password]
+      @password_retry += 1
+      if password_retry_limit_reached?
+        reset_connection!
+        consolize("Password invalid!=br=Disconnected")
+      else
+        consolize("Password invalid!") << "<script>$('#delegate').val('check_password');</script>".html_safe
+      end
+    else
+      @authenticated = true
+      @password_retry = 0
+      consolize("Password valid=br==br=Connection to #{victim[:ip]} established")
     end
   end
 end
