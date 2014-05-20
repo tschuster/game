be09a734c9816475b5e5782534f8ff9f92e75b2f
Thies Schuster <ts@derschuster.de>
2014-05-20 20:09:52 +0200

    cleanup

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,16 +1,4 @@
 module JobsHelper
-  def job_image_tag(job)
-    source = case job.type_id
-      when Job::JOB_TYPE_DDOS
-        "ddos_attack"
-      when Job::JOB_TYPE_SPAM
-        "spam"
-      when Job::JOB_TYPE_DEFACEMENT
-        "defacement"
-    end
-    image_tag("#{source}.png", :'no_pix' => true)
-  end
-
   def job_requirement(job)
     result = ""
     if job.hacking_ratio_required > 0
@@ -71,13 +59,5 @@ module JobsHelper
       result << link_to("accept", accept_job_path(job.id), class: "btn btn-primary", style: "font-size: 9px;")
     end
     result.html_safe
-  end
-
-  def image_for(job)
-    if current_user.has_incomplete_actions? || current_user.hacking_ratio < job.hacking_ratio_required || current_user.botnet_ratio < job.botnet_ratio_required
-      job_image_tag(job)
-    else
-      link_to(job_image_tag(job), accept_job_path(job.id))
-    end
   end
 end
