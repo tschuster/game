110d43f44f1f4d0925677297bd166d36afc3c50a
Thies Schuster <ts@derschuster.de>
2014-05-21 14:34:18 +0200

    bugfixing action buttons

--- app/helpers/actions_helper.rb
+++ app/helpers/actions_helper.rb
@@ -6,18 +6,24 @@ module ActionsHelper
       Action::TYPE_HACKING_BUY,
       Action::TYPE_DEFENSE_BUY
     ].include?(action_id)
-      if current_user.can_buy?(action_id)
+      if @current_action.present? && !@current_action.can_be_canceled?
+        result = '<span class="btn btn-default disabled">System down</span>'
+      elsif current_user.can_buy?(action_id)
         result = link_to(actions_path(type_id: action_id), method: :post, class: "btn btn-primary") do
-          '<span class="glyphicon glyphicon-usd" style="width: 61px;"> Buy</span>'.html_safe
+          '<span class="glyphicon glyphicon-usd" style="width: 61px;"> Buy</span>'
         end
       else
-        result = '<span class="btn btn-default disabled">Insufficent Funds</span>'.html_safe
+        result = '<span class="btn btn-default disabled">Insufficent Funds</span>'
       end
-      return result
+      return result.html_safe
     end
 
     if @current_action.present?
-      result = '<span class="btn btn-default disabled">Action pending</span>'
+      if @current_action.can_be_canceled?
+        result = '<span class="btn btn-default disabled">Action pending</span>'
+      else
+        result = '<span class="btn btn-default disabled">System down</span>'
+      end
     else
       result = link_to(actions_path(type_id: action_id), method: :post, class: "btn btn-primary") do
         '<span class="glyphicon glyphicon-collapse-up" style="width: 61px;"> Train</span>'.html_safe
